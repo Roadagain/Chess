91c7109c2cea01aae94c286d9f026110c446bdf7
Roadagain <ringoh72@gmail.com>
2016-10-22 00:02:50 +0900

    Move pieces follows input

--- src/chess/main.go
+++ src/chess/main.go
@@ -2,9 +2,27 @@ package main
 
 import (
 	"chessboard"
+	"fmt"
 )
+
+func scanMove() (*chessboard.Move, error) {
+	var file byte
+	var rank int
+
+	_, err := fmt.Scanf("%c %d", &file, &rank)
+	return chessboard.NewMove(file, rank), err
+}
 
 func main() {
 	board := chessboard.NewBoard()
-	board.Print()
+
+	for true {
+		board.Print()
+		from, err := scanMove()
+		if err != nil {
+			break
+		}
+		to, _ := scanMove()
+		board.Move(from.ToPoint(), to.ToPoint())
+	}
 }
