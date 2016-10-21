6c83d00995cf4d3307c814ca6744be5cb65bf6f2
Roadagain <ringoh72@gmail.com>
2016-10-21 17:54:49 +0900

    Set new Board and its initializer

--- src/chessboard/piece.go
+++ src/chessboard/piece.go
@@ -1,16 +1,24 @@
 package chessboard
 
-type Board [][]byte
+var starting = [8][8]byte{
+	{'R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'},
+	{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+	{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+	{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+	{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+	{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+	{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+	{'R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'},
+}
 
-var starting = Board{
-	{'R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'},
-	{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-	{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-	{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-	{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-	{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-	{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-	{'R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'},
+type Board struct {
+	matrix [8][8]byte
+}
+
+func NewBoard() *Board {
+	board := new(Board)
+	board.matrix = starting
+	return board
 }
 
 func Piece(p Point) byte {
