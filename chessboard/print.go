e7b32d1947b45d3caa26d9ee5e6294b426aa549f
Roadagain <ringoh72@gmail.com>
2016-10-21 09:43:41 +0900

    Print coordinates

--- chessboard/print.go
+++ chessboard/print.go
@@ -3,21 +3,33 @@ package chessboard
 import "fmt"
 
 func PrintChessboard() {
+	fmt.Print(" ")
 	for i := 0; i < 8; i++ {
-		fmt.Print("+")
+		fmt.Printf(" %c", 'a'+i)
+	}
+	fmt.Println()
+
+	for i := 0; i < 8; i++ {
+		fmt.Print(" +")
 		for j := 0; j < 8; j++ {
 			fmt.Print("-+")
 		}
 		fmt.Println()
-		fmt.Print("|")
+		fmt.Printf("%d|", i+1)
 		for j := 0; j < 8; j++ {
 			fmt.Print(" |")
 		}
-		fmt.Println()
+		fmt.Println(i + 1)
 	}
-	fmt.Print("+")
+	fmt.Print(" +")
 	for i := 0; i < 8; i++ {
 		fmt.Print("-+")
+	}
+	fmt.Println()
+
+	fmt.Print(" ")
+	for i := 0; i < 8; i++ {
+		fmt.Printf(" %c", 'a'+i)
 	}
 	fmt.Println()
 }
