c3ba1eccfc12648fbfdac31e4e60cac87fcca99e
Roadagain <ringoh72@gmail.com>
2016-10-21 18:00:40 +0900

    Move printing chessboard as method of Board

--- src/chessboard/board.go
+++ src/chessboard/board.go
@@ -1,5 +1,7 @@
 package chessboard
 
+import "fmt"
+
 var starting = [8][8]byte{
 	{'R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'},
 	{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
@@ -23,4 +25,36 @@ func NewBoard() *Board {
 
 func (board Board) Piece(p Point) byte {
 	return board.matrix[p.y][p.x]
+}
+
+func (board Board) Print() {
+	fmt.Print(" ")
+	for i := 0; i < 8; i++ {
+		fmt.Printf(" %c", 'a'+i)
+	}
+	fmt.Println()
+
+	for i := 0; i < 8; i++ {
+		fmt.Print(" +")
+		for j := 0; j < 8; j++ {
+			fmt.Print("-+")
+		}
+		fmt.Println()
+		fmt.Printf("%d|", i+1)
+		for j := 0; j < 8; j++ {
+			fmt.Printf("%c|", board.Piece(Point{i, j}))
+		}
+		fmt.Println(i + 1)
+	}
+	fmt.Print(" +")
+	for i := 0; i < 8; i++ {
+		fmt.Print("-+")
+	}
+	fmt.Println()
+
+	fmt.Print(" ")
+	for i := 0; i < 8; i++ {
+		fmt.Printf(" %c", 'a'+i)
+	}
+	fmt.Println()
 }
