86e184f816ce72066b59372f47659f3d7c5a90b6
Roadagain <ringoh72@gmail.com>
2016-10-21 17:06:03 +0900

    Get starting pieces out of functions

--- chessboard/piece.go
+++ chessboard/piece.go
@@ -1,16 +1,16 @@
 package chessboard
 
-func Piece(p Point) byte {
-	starting := [][]byte{
-		[]byte{'R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'},
-		[]byte{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-		[]byte{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		[]byte{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		[]byte{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		[]byte{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		[]byte{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-		[]byte{'R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'},
-	}
+var starting = [][]byte{
+	[]byte{'R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'},
+	[]byte{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+	[]byte{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+	[]byte{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+	[]byte{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+	[]byte{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+	[]byte{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+	[]byte{'R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'},
+}
 
+func Piece(p Point) byte {
 	return starting[p.y][p.x]
 }
