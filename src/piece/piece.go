6eabff3a7b09421afae2ffbdb18a5958a3e95cce
Roadagain <ringoh72@gmail.com>
2016-10-24 17:55:54 +0900

    Implement can-move check to piece

--- src/piece/piece.go
+++ src/piece/piece.go
@@ -1,10 +1,26 @@
 package piece
 
-import "point"
+import (
+	"chessboard"
+	"point"
+)
 
 type Limit [3][3]int
 
 type Piece struct {
 	movable []point.Point
 	limit   Limit
+}
+
+func (piece Piece) CanMove(from, to point.Point) bool {
+	if chessboard.InBoard(from) == false || chessboard.InBoard(to) == false {
+		return false
+	}
+	diff := from.Diff(to)
+	for i := 0; i < len(piece.movable); i++ {
+		if diff.Y <= piece.movable[i].Y && diff.X <= piece.movable[i].X {
+			return true
+		}
+	}
+	return false
 }
