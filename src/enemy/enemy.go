bea489719d2e44db7dc8ab2c6cf9d9cf335348bb
Roadagain <ringoh72@gmail.com>
2016-11-02 22:46:22 +0900

    Add enemy structure

---
+++ src/enemy/enemy.go
@@ -0,0 +1,18 @@
+package enemy
+
+import (
+	"board"
+	"color"
+)
+
+type Enemy struct {
+	board.Board
+	color.Color
+}
+
+func NewEnemy(b board.Board, c color.Color) *Enemy {
+	enemy := new(Enemy)
+	enemy.Board = b
+	enemy.Color = c
+	return enemy
+}
