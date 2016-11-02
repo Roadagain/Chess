2867515fac88917f8987e0c14e23ac6b68bc6821
Roadagain <ringoh72@gmail.com>
2016-11-02 23:45:39 +0900

    Change Enemy's members and how to get them

--- src/enemy/enemy.go
+++ src/enemy/enemy.go
@@ -3,18 +3,23 @@ package enemy
 import (
 	"board"
 	"color"
+	"matrix"
 )
 
 type Enemy struct {
-	board.Board
+	matrix.Matrix
 	color.Color
 	first [8][8]bool
 }
 
-func NewEnemy(b board.Board, c color.Color, first [8][8]bool) *Enemy {
+func NewEnemy(b *board.Board, c color.Color) *Enemy {
 	enemy := new(Enemy)
-	enemy.Board = b
+	enemy.Matrix = b.Matrix
 	enemy.Color = c
-	enemy.first = first
+	for i := 0; i < matrix.SIDE; i++ {
+		for j := 0; j < matrix.SIDE; j++ {
+			enemy.first[i][j] = b.IsFirst(matrix.Point{i, j})
+		}
+	}
 	return enemy
 }
