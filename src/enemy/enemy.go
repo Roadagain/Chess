97df6d1b75d96e223981a1fb9a0f970b3154451d
Roadagain <ringoh72@gmail.com>
2016-11-03 00:27:03 +0900

    Use board's canmove in enemy

--- src/enemy/enemy.go
+++ src/enemy/enemy.go
@@ -3,23 +3,16 @@ package enemy
 import (
 	"board"
 	"color"
-	"matrix"
 )
 
 type Enemy struct {
-	matrix.Matrix
+	board.Board
 	color.Color
-	first [8][8]bool
 }
 
 func NewEnemy(b *board.Board, c color.Color) *Enemy {
 	enemy := new(Enemy)
-	enemy.Matrix = b.Matrix
+	enemy.Board = *b
 	enemy.Color = c
-	for i := 0; i < matrix.SIDE; i++ {
-		for j := 0; j < matrix.SIDE; j++ {
-			enemy.first[i][j] = b.IsFirst(matrix.Point{i, j})
-		}
-	}
 	return enemy
 }
--- src/enemy/random.go
+++ src/enemy/random.go
@@ -4,7 +4,6 @@ import (
 	"color"
 	"math/rand"
 	"matrix"
-	"piece"
 	"time"
 )
 
@@ -14,20 +13,13 @@ func (enemy Enemy) RandomizedSelect() (matrix.Point, matrix.Point) {
 	var from, to matrix.Point
 	unsettled := true
 	mine := enemy.Matrix.Positions(enemy.Color)
-	movable := append(enemy.Matrix.Positions(color.Empty), enemy.Matrix.Positions(enemy.Color.Enemy())...)
+	movable := enemy.Board.Matrix.Positions(color.Empty)
+	movable = append(movable, enemy.Board.Matrix.Positions(enemy.Color.Enemy())...)
 
 	for _, i := range mine {
-		p := piece.WhichPiece(enemy.Matrix[i.Y][i.X])
 		for _, j := range movable {
-			var diff matrix.Point
-			if enemy.Color == color.White {
-				diff = i.Diff(j)
-			} else {
-				diff = j.Diff(i)
-			}
-
-			toEnemy := color.WhichColor(enemy.Matrix[j.Y][j.X]) == enemy.Color.Enemy()
-			if p.CanMove(diff, enemy.first[i.Y][i.X], toEnemy) && enemy.Matrix.ExistBarrier(i, j) == false {
+			canMove, _ := enemy.Board.CanMove(i, j, enemy.Color)
+			if canMove {
 				if unsettled || rand.Intn(2) == 1 {
 					from = i
 					to = j
